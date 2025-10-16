8641481a3b8f876fcaaa2bdcb23991c34e7a97f5
Malesha <malesha.jayaratne@student.curtin.edu.au>
2025-10-16 23:52:44 +0800

    Setting up some java files

---
+++ src/main/java/maleshaj/projects/javaminigame1/gameMain.java
@@ -0,0 +1,17 @@
+package maleshaj.projects.javaminigame1;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import java.io.*;
+import java.util.*;
+
+public class gameMain {
+
+    /* Is this notation correct? */
+    private static final Logger logger = Logger.getLogger(AssignmentMain.class.getName());
+
+    
+
+
+}
--- src/main/java/maleshaj/projects/javaminigame1/main.java
+++
@@ -1,7 +0,0 @@
-package maleshaj.projects.javaminigame1;
-
-import java.util.logging.Logger;
-import java.util.logging.Level;
-
-import java.io.*;
-import java.util.*;
